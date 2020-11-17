8cc247bfacbd6eac2e0705007c2dfd7a71fa5875
majes <liones2014@mail.ru>
2020-11-18 02:07:40 +0700

    base rest api get,post,delete,put

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const path = require('path')
+const {v4} = require('uuid')
 const app = express()
 
 
-const CONTACTS = [
-    {id: 1, name: 'Lion', value: '89612249552', marked: false},
+let CONTACTS = [
+    {id: v4(), name: 'Lion', value: '89612249552', marked: false}
 ]
 
-app.use(express.static(path.resolve(__dirname, 'client')))//
+app.use(express.json()) //request json
+
+//end points
+app.get('/api/contacts', (req, res) => {
+    res.status(200).json(CONTACTS)
+})
+
+app.post('/api/contacts', (req, res) => {
+    const contact = {...req.body, id: v4(), marked: false}
+    CONTACTS.push(contact)
+    res.status(201).json(contact)
+});
+
+app.delete('/api/contacts/:id', (req, res) => {
+    CONTACTS = CONTACTS.filter(c => c.id !== req.params.id);
+    res.status(200).json({message: 'Контакт был удален'})
+})
+
+app.put('/api/contacts/:id', (req, res) => {
+    const index = CONTACTS.findIndex(c => c.id === req.params.id)
+
+    CONTACTS[index] = req.body
+    res.status(200).json(CONTACTS[index])
+})
+
+app.use(express.static(path.resolve(__dirname, 'client')))
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
--- client/frontend.js
+++ client/frontend.js
@@ -1,9 +1,20 @@
 import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js"
 
+Vue.component('loader', {
+    template: `
+    <div style="display: flex; justify-content: center; align-items: center">
+    <div  class="spinner-border" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>
+    </div>
+`
+})
+
 new Vue({
     el: '#app',
     data() {
         return {
+            loading: false,
             form: {
                 name: '',
                 value: ''
@@ -17,18 +28,26 @@ new Vue({
         }
     },
     methods: {
-        createContact() {
+        async createContact() {
             const {...contact} = this.form
-            this.contacts.push({...contact, id: Date.now(), marked: false})
+            const newContact = await request('/api/contacts', 'POST', contact)
+            this.contacts.push(newContact)
             this.form.value = this.form.name = ''
         },
-        markContact(id) {
+        async markContact(id) {
             const contact = this.contacts.find(c => c.id === id)
-            contact.marked = true
+            const updated = await request(`/api/contacts/${id}`, 'PUT', {...contact, marked: true})
+            contact.marked = updated.marked
         },
-        removeContact(id) {
+        async removeContact(id) {
+            await request(`/api/contacts/${id}`, 'DELETE')
             this.contacts = this.contacts.filter(c => c.id !== id)
         }
+    },
+    async mounted() {
+        this.loading = true
+        this.contacts = await request('/api/contacts')
+        this.loading = false
     }
 })
 
@@ -41,12 +60,14 @@ async function request(url, method = 'GET', data = null) {
             headers['Content-Type'] = 'application/json'
             body = JSON.stringify(data)
         }
+
         const response = await fetch(url, {
             method,
             headers,
             body
         })
         return response.json()
+
     } catch (e) {
         console.warn('Error', e.message)
     }
