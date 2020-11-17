d34c3d4b38d069ad2124251fd6ba40d5d2ddf2be
majes <liones2014@mail.ru>
2020-11-17 21:21:19 +0700

    collecting data from a form

---
+++ client/frontend.js
@@ -0,0 +1,21 @@
+import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js"
+
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            form: {
+                name: '',
+                value: ''
+            },
+            contacts: []
+        }
+    },
+    methods: {
+        createContact() {
+            const {...contact} = this.form
+            this.contacts.push({...contact, id: Date.now()})
+            this.form.value = this.form.name = ''
+        }
+    }
+})
